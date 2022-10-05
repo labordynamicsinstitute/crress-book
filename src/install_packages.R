c701cf08b2e35885e139d210f7a3a9ccd8846d26
Aleksandr Michuda <amichuda@gmail.com>
2022-10-05 13:45:39 -0400

    Update install_packages.R

--- src/install_packages.R
+++ src/install_packages.R
@@ -3,8 +3,7 @@
 pkg_list <- c("rmarkdown")
 
 for (pkg in pkg_list) {  
-  install.packages(pkg, repos = "your_cran_mirror", 
-                   lib = "R/x86_64-pc-linux-gnu-library/3.3") 
+  install.packages(pkg, repos = "https://cloud.r-project.org/") 
   if (!require(pkg, character.only = T)) {
     quit(save = "no", status = 1, runLast = FALSE)  
   }
